04d6e2e4af04ef0d4fbd1f50e8c2633408dd90dc
Deepfried Duck <juliansoarse@gmail.com>
2026-03-20 14:42:25 +0100

    .

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function toggleMenu(){
+    document.getElementById("sidebar").classList.toggle("open");
+}
 
+function closeMenu(){
+    if(window.innerWidth <= 768){
+        document.getElementById("sidebar").classList.remove("open");
+    }
+}
